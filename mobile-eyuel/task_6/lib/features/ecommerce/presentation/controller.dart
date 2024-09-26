c580fe78f85ccd2c934a211827490479ac68121e
ETjoel <ejoeltesfaye@gmail.com>
2024-09-27 00:30:52 +0300

    working on consuming bloc to ui

--- mobile-eyuel/task_6/lib/features/ecommerce/presentation/controller.dart
+++ mobile-eyuel/task_6/lib/features/ecommerce/presentation/controller.dart
@@ -1,3 +1,9 @@
 import 'package:get/get.dart';
 
-class AuthController extends GetxController {}
+class AuthController extends GetxController {
+  final showInputError = false.obs;
+
+  void toggleInputError() {
+    showInputError.value = !showInputError.value;
+  }
+}
